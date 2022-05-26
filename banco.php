6dcadbe98bc719dd5abc300ece293b150d052fe8
Giovani Gama <giovanigama.estudo@gmail.com>
2022-05-25 22:43:20 -0300

    #master - implementando um autoloader

--- banco.php
+++ banco.php
@@ -1,11 +1,14 @@
 <?php
 
-require_once 'src/Modelo/Conta/Conta.php';
-require_once 'src/Modelo/Endereco.php';
-require_once 'src/Modelo/Pessoa.php';
-require_once 'src/Modelo/Conta/Titular.php';
-require_once 'src/Modelo/CPF.php';
-require_once 'src/Modelo/Funcionario.php';
+spl_autoload_register(function (string $nomeCompletoClasse){
+    $caminhoArquivo = str_replace('Alura\\Banco', 'src',$nomeCompletoClasse);
+    $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
+    $caminhoArquivo .= '.php';
+
+    if(file_exists($caminhoArquivo)){
+        require_once $caminhoArquivo;
+    }
+});
 
 use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\Endereco;
